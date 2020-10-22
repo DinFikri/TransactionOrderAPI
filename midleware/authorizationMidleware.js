13c2b4bb593f73cb11f4cae91e154822c238522c
Din Fikri <fikridin22@gmail.com>
2020-10-23 04:57:22 +0700

    add_new_authorization_code

--- midleware/authorizationMidleware.js
+++ midleware/authorizationMidleware.js
@@ -2,12 +2,14 @@ const hyperId = require('hyperid')
 
 function authorization(req, res, next) {
     const authorization = req.headers.authorization
-    const token = authorization.split(' ')[1]
-    const isValidToken = hyperId.decode(token)
-    if (isValidToken) {
-        next()
-    } else {
-        res.status(401).send("oh tidak boleh")
+    if (authorization) {
+        const token = authorization.split(' ')[1]
+        const isValidToken = hyperId.decode(token)
+        if (isValidToken) {
+            next()
+        } else {
+            res.status(401).send("oh tidak boleh")
+        }
     }
 }
 
